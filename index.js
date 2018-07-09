58c9f6f43f53084191564a2b98425b97560a9219
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-09 21:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = neighborhoodId++;
+    store.neighborhood.push(this);
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
+}
