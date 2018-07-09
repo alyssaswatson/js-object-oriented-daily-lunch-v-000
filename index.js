d87ecbf48f51d24bc9af72b9eaf7b9e286453a33
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-09 21:56:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ class Neighborhood {
     this.id = neighborhoodId++;
     store.neighborhood.push(this);
   }
-  trips() {
-    return store.trips.filter(trip => {
-      return trip.driverId == this.id;
-    });
-  }
-  passengers() {
-    return this.trips().map(trip => {
-      return trip.passenger();
+  deliveries() {
+    return store.deliveries.filter(trip => {
+      return trip.neighborhoodId == this.id;
     });
   }
 }
